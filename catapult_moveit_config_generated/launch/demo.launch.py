ad81321ba3d4939018736f6639a302b1e2fa588b
Gregory Jones <greggie119gj@gmail.com>
2025-04-02 14:25:02 -0400

    Generated moveit config package

---
+++ catapult_moveit_config_generated/launch/demo.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_demo_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("ur5", package_name="catapult_moveit_config_generated").to_moveit_configs()
+    return generate_demo_launch(moveit_config)
