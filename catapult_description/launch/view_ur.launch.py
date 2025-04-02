c53f2176cf9bf71d283678d802c8bd366129d6b3
Gregory Jones <greggie119gj@gmail.com>
2025-04-02 11:48:21 -0400

    rename package for catapult_description

--- catapult_description/launch/view_ur.launch.py
+++ catapult_description/launch/view_ur.launch.py
@@ -83,7 +83,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_package",
-            default_value="ur_description",
+            default_value="catapult_description",
             description="Description package with robot URDF/XACRO files. Usually the argument "
             "is not set, it enables use of a custom description.",
         )
@@ -91,7 +91,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="ur.urdf.xacro",
+            default_value="ur_with_gripper.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
