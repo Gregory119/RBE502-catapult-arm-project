42a60a4089d3f0c6f2e008dc364ad26526d59103
Gregory Jones <greggie119gj@gmail.com>
2025-05-01 18:24:01 -0400

    Enlarge tolerances for bad PD controller

--- catapult_moveit_config/launch/ur_moveit.launch.py
+++ catapult_moveit_config/launch/ur_moveit.launch.py
@@ -193,7 +193,7 @@ def launch_setup(context, *args, **kwargs):
         "moveit_manage_controllers": False,
         "trajectory_execution.allowed_execution_duration_scaling": 1.2,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
-        "trajectory_execution.allowed_start_tolerance": 0.1,
+        "trajectory_execution.allowed_start_tolerance": 0.5,
         # Execution time monitoring can be incompatible with the scaled JTC
         "trajectory_execution.execution_duration_monitoring": False,
     }
