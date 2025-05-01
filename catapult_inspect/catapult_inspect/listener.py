1782ae656c4a14457dfd8a5590176825b216fb08
Gregory Jones <greggie119gj@gmail.com>
2025-05-01 18:26:10 -0400

    Plot desired and actual joint trajectories

---
+++ catapult_inspect/catapult_inspect/listener.py
@@ -0,0 +1,138 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.duration import Duration, Infinite
+import rclpy.qos as qos
+from rclpy.qos_event import SubscriptionEventCallbacks
+
+from control_msgs.action._follow_joint_trajectory import FollowJointTrajectory_FeedbackMessage
+
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+
+import signal
+import socket
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            FollowJointTrajectory_FeedbackMessage,
+            '/custom_trajectory_controller/follow_joint_trajectory/_action/feedback',
+            self.listener_callback,
+            10)
+            # qos.QoSProfile(history=qos.HistoryPolicy.KEEP_LAST,
+            #                depth=100,
+            #                reliability=qos.ReliabilityPolicy.RELIABLE,
+            #                durability=qos.DurabilityPolicy.VOLATILE,
+            #                lifespan=Infinite,
+            #                deadline=Infinite,
+            #                liveliness=qos.LivelinessPolicy.AUTOMATIC,
+            #                liveliness_lease_duration=Infinite),
+            # event_callbacks = SubscriptionEventCallbacks(deadline=self.deadline,
+            #                                              incompatible_qos=self.incompatible_qos,
+            #                                              liveliness=self.liveliness,
+            #                                              message_lost=self.msg_lost,
+            #                                              use_default_callbacks=False))
+        
+        self.subscription  # prevent unused variable warning
+        print("contructed")
+
+        self.fig, (self.ax_pos, self.ax_vel) = plt.subplots(2,1)
+        self.pos_desired_plotter = DataPlotter(self.ax_pos, 'qd', fmt='--', lw=3)
+        self.pos_actual_plotter = DataPlotter(self.ax_pos, 'q', fmt='-', lw=1.5)
+        self.vel_desired_plotter = DataPlotter(self.ax_vel, 'dqd', fmt='--', lw=3)
+        self.vel_actual_plotter = DataPlotter(self.ax_vel, 'dq', fmt='-', lw=1.5)    
+
+
+    def listener_callback(self, msg):
+        # update position subplot
+        if self.ax_pos.get_legend() is not None:
+            self.ax_pos.get_legend().remove()
+
+        self.pos_desired_plotter.update(msg.feedback.desired.positions)
+        self.pos_actual_plotter.update(msg.feedback.actual.positions)
+        self.ax_pos.set_title('Joint Positions vs Time')
+        self.ax_pos.set_xlabel('time [s]')
+        self.ax_pos.set_ylabel('position [rad]')
+        self.ax_pos.legend()
+
+        # update velocity subplot
+        if self.ax_vel.get_legend() is not None:
+            self.ax_vel.get_legend().remove()
+        self.vel_desired_plotter.update(msg.feedback.desired.velocities)
+        self.vel_actual_plotter.update(msg.feedback.actual.velocities)
+        self.ax_vel.set_title('Joint Velocities vs Time')
+        self.ax_vel.set_xlabel('time [s]')
+        self.ax_vel.set_ylabel('velocity [rad/s]')
+        self.ax_vel.legend()
+
+        plt.pause(0.001)
+
+    def savePlot(self):
+        plt.savefig('pos-plot.png')
+        print('saved plot')
+
+
+class DataPlotter:
+    def __init__(self, ax, label_desc, fmt='-', lw=1):
+        self.ax = ax
+        self.desc = label_desc
+        self.fmt = fmt
+        self.lw = lw
+        
+        self.lines = []
+        # self.labels = []
+        self.num_joints = 6
+        self.joint_data = [[] for _ in range(self.num_joints)]
+
+    def update(self, new_joint_data):
+        for i in range(self.num_joints):
+            self.joint_data[i].append(new_joint_data[i])
+
+        # remove lines from plot
+        for line in self.lines:
+            line.remove()
+        self.lines = []
+
+        # create new lines for plot
+        for i in range(self.num_joints):
+            x = range(len(self.joint_data[i]))
+            y = self.joint_data[i]
+            line = self.ax.plot(x, y, self.fmt, linewidth=self.lw, label='{}[{}]'.format(self.desc, i))[0]
+            self.lines.append(line)
+            # if len(self.labels) < self.num_joints:
+            #     self.labels.append('{}[{}]'.format(self.desc, i))
+
+        # reset legend
+        # self.ax.legend(self.lines, self.labels)
+        
+
+minimal_subscriber = None
+def keyboardInt(sig, frame):
+    print("keyboardInt")
+    # save the graph
+    if minimal_subscriber is not None:
+        minimal_subscriber.savePlot()
+    
+
+def main(args=None):
+    signal.signal(signal.SIGINT, keyboardInt)
+    
+    rclpy.init(args=args)
+
+    global minimal_subscriber
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
