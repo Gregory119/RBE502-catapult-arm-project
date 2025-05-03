5360a7b2f64ebc4692f1433203dc9d8ebc3f9072
Gregory Jones <greggie119gj@gmail.com>
2025-05-03 17:42:19 -0400

    Add error plots

--- catapult_inspect/catapult_inspect/listener.py
+++ catapult_inspect/catapult_inspect/listener.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 import signal
 import socket
+import numpy as np
 
 
 class MinimalSubscriber(Node):
@@ -31,7 +32,10 @@ class MinimalSubscriber(Node):
         self.pos_desired_plotter = DataPlotter(self.ax_pos, 'qd', fmt='--', lw=3)
         self.pos_actual_plotter = DataPlotter(self.ax_pos, 'q', fmt='-', lw=1.5)
         self.vel_desired_plotter = DataPlotter(self.ax_vel, 'dqd', fmt='--', lw=3)
-        self.vel_actual_plotter = DataPlotter(self.ax_vel, 'dq', fmt='-', lw=1.5)    
+        self.vel_actual_plotter = DataPlotter(self.ax_vel, 'dq', fmt='-', lw=1.5)
+
+        self.pos_err_plotter = DataPlotter(self.ax_pos_err, 'e', lw=2)
+        self.vel_err_plotter = DataPlotter(self.ax_vel_err, 'de', lw=2)
 
 
     def listener_callback(self, msg):
@@ -44,7 +48,7 @@ class MinimalSubscriber(Node):
         self.ax_pos.set_title('Joint Positions vs Time')
         self.ax_pos.set_xlabel('time [s]')
         self.ax_pos.set_ylabel('position [rad]')
-        self.ax_pos.legend()
+        self.ax_pos.legend(loc='upper left')
 
         # update velocity subplot
         if self.ax_vel.get_legend() is not None:
@@ -56,6 +60,27 @@ class MinimalSubscriber(Node):
         self.ax_vel.set_ylabel('velocity [rad/s]')
         self.ax_vel.legend()
 
+        # update position error subplot
+        if self.ax_pos_err.get_legend() is not None:
+            self.ax_pos_err.get_legend().remove()
+        pos_errs = np.array(msg.feedback.desired.positions)-np.array(msg.feedback.actual.positions)
+        self.pos_err_plotter.update(pos_errs, msg.feedback.header.stamp)
+        self.ax_pos_err.set_title('Joint Position Errors vs Time')
+        self.ax_pos_err.set_xlabel('time [s]')
+        self.ax_pos_err.set_ylabel('position error [rad]')
+        self.ax_pos_err.legend()
+
+        # update velocity error subplot
+        if self.ax_vel_err.get_legend() is not None:
+            self.ax_vel_err.get_legend().remove()
+        vel_errs = np.array(msg.feedback.desired.velocities)-np.array(msg.feedback.actual.velocities)
+        self.vel_err_plotter.update(vel_errs, msg.feedback.header.stamp)
+        self.ax_vel_err.set_title('Joint Velocity Errors vs Time')
+        self.ax_vel_err.set_xlabel('time [s]')
+        self.ax_vel_err.set_ylabel('velocity error [rad/s]')
+        self.ax_vel_err.legend()
+
+        
         plt.pause(0.001)
 
     def savePlot(self):
@@ -75,10 +100,14 @@ class DataPlotter:
         self.num_joints = 6
         self.joint_data = [[] for _ in range(self.num_joints)]
         self.time_data = [] # seconds since start
+        self.start_time = None
 
     def update(self, new_joint_data, time_from_start):
         time = Time.from_msg(time_from_start)
-        self.time_data.append(float(time.nanoseconds)*1e-6)
+        if self.start_time is None:
+            self.start_time = time
+        
+        self.time_data.append((float(time.nanoseconds) - float(self.start_time.nanoseconds))*1e-9)
         for i in range(self.num_joints):
             self.joint_data[i].append(new_joint_data[i])
 
